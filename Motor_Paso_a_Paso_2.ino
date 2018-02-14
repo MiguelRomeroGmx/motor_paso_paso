4313b6cb84e8e807f86b65ac3c8887e7ea9e8c79
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-02-14 12:44:28 -0600

    Motot paso a paso

---
+++ Motor_Paso_a_Paso_2.ino
@@ -0,0 +1,88 @@
+
+
+int retardo=2;          // Tiempo de retardo en milisegundos (Velocidad del Motor)
+
+
+void setup() {                
+pinMode(11, OUTPUT);    // Pin 11 conectar a IN4
+pinMode(10, OUTPUT);    // Pin 10 conectar a IN3
+pinMode(9, OUTPUT);     // Pin 9 conectar a IN2
+pinMode(8, OUTPUT);     // Pin 8 conectar a IN1
+pinMode(2, INPUT);
+pinMode(3, INPUT);
+}
+
+void loop() {
+
+if (digitalRead(2)==HIGH){
+paso_der();
+}
+else{
+  
+if (digitalRead(3)==HIGH){
+  paso_izq();
+}
+else{
+ 
+apagado(); 
+}
+}
+
+
+  
+  
+}  ///////////////////// Fin del Loop ///////////////////////////
+
+void paso_der(){         // Pasos a la derecha
+ digitalWrite(11, LOW); 
+ digitalWrite(10, LOW);  
+ digitalWrite(9, HIGH);  
+ digitalWrite(8, HIGH);  
+   delay(retardo); 
+ digitalWrite(11, LOW); 
+ digitalWrite(10, HIGH);  
+ digitalWrite(9, HIGH);  
+ digitalWrite(8, LOW);  
+   delay(retardo); 
+ digitalWrite(11, HIGH); 
+ digitalWrite(10, HIGH);  
+ digitalWrite(9, LOW);  
+ digitalWrite(8, LOW);  
+  delay(retardo); 
+ digitalWrite(11, HIGH); 
+ digitalWrite(10, LOW);  
+ digitalWrite(9, LOW);  
+ digitalWrite(8, HIGH);  
+  delay(retardo);  
+}
+
+void paso_izq() {        // Pasos a la izquierda
+ digitalWrite(11, HIGH); 
+ digitalWrite(10, HIGH);  
+ digitalWrite(9, LOW);  
+ digitalWrite(8, LOW);  
+  delay(retardo); 
+ digitalWrite(11, LOW); 
+ digitalWrite(10, HIGH);  
+ digitalWrite(9, HIGH);  
+ digitalWrite(8, LOW);  
+  delay(retardo); 
+ digitalWrite(11, LOW); 
+ digitalWrite(10, LOW);  
+ digitalWrite(9, HIGH);  
+ digitalWrite(8, HIGH);  
+  delay(retardo); 
+ digitalWrite(11, HIGH); 
+ digitalWrite(10, LOW);  
+ digitalWrite(9, LOW);  
+ digitalWrite(8, HIGH);  
+  delay(retardo); 
+}
+        
+void apagado() {         // Apagado del Motor
+ digitalWrite(11, LOW); 
+ digitalWrite(10, LOW);  
+ digitalWrite(9, LOW);  
+ digitalWrite(8, LOW);  
+ }
+
